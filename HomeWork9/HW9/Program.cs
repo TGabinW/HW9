271630856ec5afc816504948518de83887346b94
TGabinW <111113016+TGabinW@users.noreply.github.com>
2022-10-02 12:11:57 +0300

    Add files via upload

---
+++ HomeWork9/HW9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+using ConsoleApp1;
+
+namespace Host;
+
+internal static class Program
+{
+    private static async Task Main(string[] args)
+    {
+
+        foreach (var arg in args) Console.WriteLine(arg);
+
+        string pathCatalog = args[0];
+        string pathToReport = args[1];
+        byte searchDepth = byte.Parse(args[2]);
+
+        double size = 0;
+        DateTime min, max, reportDate;
+
+        if (Directory.Exists(pathCatalog))
+        {
+            ReportItem.sizeOfFolder(pathCatalog, ref size);
+            min = Directory.GetCreationTime(pathCatalog);
+            max = Directory.GetLastWriteTime(pathCatalog);
+
+            ReportItem report = new ReportItem(size, min, max, reportDate = DateTime.Now);
+
+            Console.WriteLine(report);
+
+            using (FileStream fs = new FileStream(pathToReport, FileMode.OpenOrCreate))
+            {
+                await JsonSerializer.SerializeAsync<ReportItem>(fs, report);
+                Console.WriteLine($"Информация сохранена в файл {pathToReport}.");
+            }
+        }
+        else
+        {
+            min = DateTime.MinValue;
+            max = DateTime.MinValue;
+            size = 0;
+            Console.WriteLine("Каталога не существует.");
+        }
+
+
+    }
+}
---
+++ HomeWork9/Models/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConsoleApp1;
+
+public class ReportItem
+{
+    [JsonPropertyName("Catalog size")]
+    public double size { get; set; }
+    [JsonPropertyName("Create date")]
+    public DateTime min { get; set; }
+    [JsonPropertyName("Last change date")]
+    public DateTime max { get; set; }
+    [JsonPropertyName("Report create date")]
+    public DateTime reportDate { get; set; }
+
+    public ReportItem(double size, DateTime min, DateTime max, DateTime reportDate)
+    {
+        this.size = size;
+        this.min = min;
+        this.max = max;
+        this.reportDate = reportDate;
+    }
+
+    public override string ToString()
+    {
+        return $"Размер: {size}, дата создания: {min}, дата последнего изменения: {max}, дата создания отчета: {reportDate}.";
+    }
+
+    public static double sizeOfFolder(string folder, ref double catalogSize)
+    {
+        try
+        {
+            DirectoryInfo di = new DirectoryInfo(folder);
+            DirectoryInfo[] diA = di.GetDirectories();
+            FileInfo[] fi = di.GetFiles();
+            foreach (FileInfo f in fi)
+            {
+                catalogSize = catalogSize + f.Length;
+            }
+            foreach (DirectoryInfo df in diA)
+            {
+                sizeOfFolder(df.FullName, ref catalogSize);
+            }
+            return Math.Round((double)(catalogSize / 1024 / 1024 / 1024), 1);
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            Console.WriteLine("Директория не найдена. Ошибка: " + ex.Message);
+            return 0;
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.WriteLine("Отсутствует доступ. Ошибка: " + ex.Message);
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Произошла ошибка. Обратитесь к администратору. Ошибка: " + ex.Message);
+            return 0;
+        }
+    }
+}
